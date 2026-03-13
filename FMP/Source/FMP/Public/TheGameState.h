a6f17a5ce2e588d79ca1332748d1edae5a595c66
SiddPlus <sidd.ghosalkar@outlook.com>
2026-03-13 10:27:49 +0000

    Added TheGameState & TheGameMode

---
+++ FMP/Source/FMP/Public/TheGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "TheGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FMP_API ATheGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
