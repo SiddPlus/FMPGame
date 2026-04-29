68718465357781749823e7a70a849bae271ae9ac
SiddPlus <sidd.ghosalkar@outlook.com>
2026-04-29 13:48:29 +0100

    build fixed maybe

--- FMP/Source/FMP/FMP.Build.cs
+++ FMP/Source/FMP/FMP.Build.cs
@@ -1,55 +1,49 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
 
 public class FMP : ModuleRules
 {
-	public FMP(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public FMP(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core",
-			"CoreUObject",
-			"Engine",
-			"InputCore",
-			"Json",
-			"EnhancedInput",
-			"AIModule",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
-			"UMG",
-			"Slate"
-		});
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "Json",
+            "EnhancedInput",
+            "AIModule",
+            "StateTreeModule",
+            "GameplayStateTreeModule",
+            "UMG",
+            "Slate",
+            "NavigationSystem" // Added from your second list
+        });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { });
+        PrivateDependencyModuleNames.AddRange(new string[] { 
+            "ProceduralMeshComponent",
+            "OnlineSubsystem" // Added to support online features
+        });
 
-		PublicIncludePaths.AddRange(new string[] {
-			"FMP",
-			"FMP/Variant_Platforming",
-			"FMP/Variant_Platforming/Animation",
-			"FMP/Variant_Combat",
-			"FMP/Variant_Combat/AI",
-			"FMP/Variant_Combat/Animation",
-			"FMP/Variant_Combat/Gameplay",
-			"FMP/Variant_Combat/Interfaces",
-			"FMP/Variant_Combat/UI",
-			"FMP/Variant_SideScrolling",
-			"FMP/Variant_SideScrolling/AI",
-			"FMP/Variant_SideScrolling/Gameplay",
-			"FMP/Variant_SideScrolling/Interfaces",
-			"FMP/Variant_SideScrolling/UI"
-		});
+        // This is where you add the line you asked about:
+        DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
 
-        PrivateDependencyModuleNames.AddRange(new string[] { "ProceduralMeshComponent" });
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem" });
-
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        PublicIncludePaths.AddRange(new string[] {
+            "FMP",
+            "FMP/Variant_Platforming",
+            "FMP/Variant_Platforming/Animation",
+            "FMP/Variant_Combat",
+            "FMP/Variant_Combat/AI",
+            "FMP/Variant_Combat/Animation",
+            "FMP/Variant_Combat/Gameplay",
+            "FMP/Variant_Combat/Interfaces",
+            "FMP/Variant_Combat/UI",
+            "FMP/Variant_SideScrolling",
+            "FMP/Variant_SideScrolling/AI",
+            "FMP/Variant_SideScrolling/Gameplay",
+            "FMP/Variant_SideScrolling/Interfaces",
+            "FMP/Variant_SideScrolling/UI"
+        });
     }
 }
